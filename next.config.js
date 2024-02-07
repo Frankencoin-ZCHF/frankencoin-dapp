d065c0834e7b30ccaa4b213308d86ae57350bf1e
wany-pengy <holypengy@gmail.com>
2024-02-07 23:34:50 +1000

    feat: add test dynamic page

--- next.config.js
+++ next.config.js
@@ -1,30 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: "/",
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
 };
 
 module.exports = nextConfig;
