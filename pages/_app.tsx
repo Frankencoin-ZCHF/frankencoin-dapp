82bccc7f977581d05b39d54a5902394384ae564c
Luzius Meisser <luzius@aktionariat.com>
2024-03-28 23:43:21 +0100

    new ponder instance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import { Web3Modal } from "@components/Web3Modal";
 import { useTokenPrice, useTokenPrices } from "@hooks";
 
 const apolloClient = new ApolloClient({
-  uri: "https://ponder.frankencoin.com/",
+  uri: "https://ponder2.frankencoin.com/",
   cache: new InMemoryCache(),
 });
 
