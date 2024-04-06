7e3cf47fae0702c2ddfeb36e65c29758039f22da
Luzius Meisser <luzius@aktionariat.com>
2024-04-06 16:18:05 +0200

    walletconnect account

--- components/Web3Modal.tsx
+++ components/Web3Modal.tsx
@@ -3,23 +3,24 @@
 import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 
 import { WagmiConfig } from "wagmi";
-import { mainnet, sepolia } from "viem/chains";
+import { mainnet } from "viem/chains";
 import React from "react";
 
 // 1. Get projectId at https://cloud.walletconnect.com
-const projectId = "75da506ed9c39c840e6c5a5180014870";
+const projectId = "26fb3341cffa779adebdb59dc32b24e5";
 
-const chains = [mainnet, sepolia];
+const chains = [mainnet];
 
 // 2. Create wagmiConfig
 const metadata = {
-  name: "Web3Modal",
-  description: "Web3Modal Example",
-  url: "https://web3modal.com",
+  name: "Frankencoin",
+  description: "Frankencoin Frontend",
+  url: "https://frankencoin.com",
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+// https://docs.walletconnect.com/web3modal/nextjs/wagmi/about/implementation#wagmi-config
+const wagmiConfig = defaultWagmiConfig({ chains, ssr: true, projectId, metadata });
 
 // 3. Create modal
 createWeb3Modal({ wagmiConfig, projectId, chains });
