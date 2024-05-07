3403979a53a34f3bcd22541f9893b1396c69708a
samclassix <samclassix@proton.me>
2024-05-07 15:14:02 +0200

    fixes wrong network via useIsWrongNetwork hook

--- components/WalletConnect.tsx
+++ components/WalletConnect.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useIsWrongNetwork } from "@hooks";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+
 export default function WalletConnect() {
+  const { open } = useWeb3Modal();
+  const isWrongNetwork = useIsWrongNetwork();
+
+  useEffect(() => {
+    console.log(`Is Ethereum Mainnet: ${!isWrongNetwork}`);
+    if (!isWrongNetwork) return;
+    else open({ view: "Networks" });
+  }, [isWrongNetwork]);
+
   return (
-    <div className="flex items-center gap-4">
+    // removed: flex-col items-end sm:flex-row sm:
+    <div className="flex sm:flex-row items-center gap-4">
       <div className="flex items-center gap-2 font-bold">
+        <w3m-network-button disabled={isWrongNetwork} />
         <w3m-button />
       </div>
     </div>
