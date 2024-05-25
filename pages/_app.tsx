ee752a3468666709dfed7f1d08b6f5749c5b255a
samclassix <96245183+samclassix@users.noreply.github.com>
2024-05-25 11:43:02 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import { Web3Modal } from "@components/Web3Modal";
 import { useTokenPrice, useTokenPrices } from "@hooks";
 
 const apolloClient = new ApolloClient({
-  uri: "https://mainnetponder.frankencoin.com/",
+  uri: "https://maindevponder.frankencoin.com/",
   cache: new InMemoryCache(),
 });
 
