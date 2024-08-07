20b0e21a38c8b559532241a45cfc0b659d49c9f1
samclassix <samclassix@proton.me>
2024-08-07 09:27:36 +0200

    feat: dynamic docs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faBook, faBookmark, faComments, faCodeCommit } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faTelegram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { SubmitIssue } from "./LoadingScreen";
+import { usePathname } from "next/navigation";
 
 interface ButtonProps {
 	link: string;
@@ -21,12 +22,27 @@ const FooterButton = ({ link, text, icon }: ButtonProps) => {
 	);
 };
 
+const DynamicDocs = (): string => {
+	const p = usePathname();
+	let link: string = SOCIAL.Docs;
+
+	if (p === "/mint") link += "/positions/open";
+	else if (p === "/mypositions") link += "/positions/adjust";
+	else if (p === "/monitoring") link += "/positions/auctions";
+	else if (p === "/challenges") link += "/positions/auctions";
+	else if (p === "/pool") link += "/pool-shares";
+	else if (p === "/governance") link += "/governance";
+	else if (p === "/swap") link += "/swap";
+
+	return link;
+};
+
 export default function Footer() {
 	return (
 		<>
 			<ul className="mt-12 mb-4 flex items-center justify-center gap-8">
 				<li>
-					<FooterButton link={SOCIAL.Docs} text="Doc" icon={faBook} />
+					<FooterButton link={DynamicDocs()} text="Doc" icon={faBook} />
 				</li>
 				<li>
 					<FooterButton link={SOCIAL.SubStack} text="Blog" icon={faBookmark} />
