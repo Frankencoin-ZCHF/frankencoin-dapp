db89c31f0ae6319b22ca26a7258bc5c518d68909
samclassix <samclassix@proton.me>
2024-08-13 11:14:56 +0200

    feat: tailwind dynamic screen size adjustments

--- components/Table/TableRow.tsx
+++ components/Table/TableRow.tsx
@@ -11,18 +11,18 @@ interface Props {
 export default function TableRow({ colSpan, children, actionCol, link }: Props) {
 	return (
 		<Link
-			className={`cursor-default bg-tableRow-primary hover:bg-tableRow-hover px-8 py-4 lg:px-12 first:border-t-0 sm:first:border-t border-t border-tableRow-hover first:rounded-t-lg sm:first:rounded-t-none last:rounded-b-lg duration-300`}
+			className={`cursor-default bg-tableRow-primary hover:bg-tableRow-hover px-8  xl:px-12 py-4 first:border-t-0 sm:first:border-t border-t border-tableRow-hover first:rounded-t-lg sm:first:rounded-t-none last:rounded-b-lg duration-300`}
 			href={link || "#"}
 		>
-			<div className="flex pl-8 flex-col justify-between gap-y-5 md:flex-row">
+			<div className="flex sm:pl-8 flex-col justify-between gap-y-5 md:flex-row">
 				<div
-					className={`text-right grid flex-grow grid-cols-1 gap-3 sm:grid-cols-${
+					className={`sm:text-right max-sm:text-center grid flex-grow grid-cols-1 sm:grid-cols-${
 						colSpan || children.length
 					} max-md:justify-items-center items-center max-md:gap-6`}
 				>
 					{children}
 				</div>
-				{actionCol && <div className="flex-shrink-0 md:w-[8rem] md:ml-[2rem] my-2">{actionCol}</div>}
+				{actionCol && <div className="flex-shrink-0 md:w-[8rem] md:ml-[2rem] max-md:w-full my-2">{actionCol}</div>}
 			</div>
 		</Link>
 	);
