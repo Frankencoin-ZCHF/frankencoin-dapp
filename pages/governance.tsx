d4fe006bc5e64a0499178f3f422a32655f398f8f
samclassix <samclassix@proton.me>
2024-10-08 14:41:28 +0200

    feat: Telegram Bot description and infos

--- pages/governance.tsx
+++ pages/governance.tsx
@@ -4,6 +4,7 @@ import { shortenAddress } from "@utils";
 import GovernancePositionsTable from "@components/PageGovernance/GovernancePositionsTable";
 import GovernanceMintersTable from "@components/PageGovernance/GovernanceMintersTable";
 import GovernanceVotersTable from "@components/PageGovernance/GovernanceVotersTable";
+import GovernanceTelegramBot from "@components/PageGovernance/GovernanceTelegramBot";
 
 export default function Governance() {
 	return (
@@ -47,14 +48,14 @@ export default function Governance() {
 			<div className="md:mt-8">
 				<GovernanceVotersTable />
 			</div>
-		</>
-	);
-}
 
-function DisplayWarningMessage(props: { overwrite: Address }) {
-	return (
-		<div>
-			<span className="font-bold text-sm">{props.overwrite ? `(Public View for: ${shortenAddress(props.overwrite)})` : ""}</span>
-		</div>
+			<div className="md:mt-10">
+				<span className="font-bold text-xl">Frankencoin Api Bot</span>
+			</div>
+
+			<div className="md:mt-8">
+				<GovernanceTelegramBot />
+			</div>
+		</>
 	);
 }
