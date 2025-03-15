a51919e44f7445613b3a19e1e00d40b9252b6e79
Danswar <48102227+Danswar@users.noreply.github.com>
2025-03-15 13:45:06 -0300

    Add language detection configuration to next-i18next (#90) (#90)

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,4 +7,8 @@ module.exports = {
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
     load: 'languageOnly',
+    detection: {
+      order: ['cookie', 'localStorage', 'path', 'htmlTag'],
+      caches: ['cookie', 'localStorage']
+    }
   }
