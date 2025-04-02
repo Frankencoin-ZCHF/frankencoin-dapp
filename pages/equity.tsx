fb2640ef74f0cf52ea7cf7401dd5a123895183c9
samclassix <samclassix@proton.me>
2025-04-02 18:23:48 +0200

    feat: added fps earning table if connected wallet

--- pages/equity.tsx
+++ pages/equity.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import EquityFPSDetailsCard from "@components/PageEquity/EquityFPSDetailsCard";
 import EquityInteractionCard from "@components/PageEquity/EquityInteractionCard";
 import AppTitle from "@components/AppTitle";
+import { useAccount } from "wagmi";
+import { FPSBalanceHistory } from "../hooks/FPSBalanceHistory";
+import { FPSEarningsHistory } from "../hooks/FPSEarningsHistory";
+import ReportsFPSYearlyTable from "@components/PageReports/ReportsFPSYearlyTable";
+import { zeroAddress } from "viem";
 
 export default function Equity() {
+	const { address } = useAccount();
+	const [fpsHistory, setFpsHistory] = useState<FPSBalanceHistory[]>([]);
+	const [fpsEarnings, setFpsEarnings] = useState<FPSEarningsHistory[]>([]);
+
+	useEffect(() => {
+		if (address == undefined) {
+			setFpsHistory([]);
+			setFpsEarnings([]);
+			return;
+		}
+
+		const fetcher = async () => {
+			try {
+				const responseBalance = await FPSBalanceHistory(address);
+				setFpsHistory(responseBalance.reverse());
+
+				const responseEarnings = await FPSEarningsHistory(address);
+				setFpsEarnings(responseEarnings.reverse());
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		fetcher();
+	}, [address]);
+
 	return (
 		<>
 			<Head>
@@ -19,6 +50,13 @@ export default function Equity() {
 					<EquityFPSDetailsCard />
 				</section>
 			</div>
+
+			{address != undefined && (
+				<>
+					<AppTitle title="FPS Holder Earnings" />
+					<ReportsFPSYearlyTable address={address || zeroAddress} fpsHistory={fpsHistory} fpsEarnings={fpsEarnings} />
+				</>
+			)}
 		</>
 	);
 }
