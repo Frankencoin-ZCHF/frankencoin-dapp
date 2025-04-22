e83a8c7b240db3cbb2a52983b5597bd4f8c69b87
samclassix <samclassix@proton.me>
2025-04-22 12:19:16 +0200

    feat: footer cross device fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,7 +29,7 @@ const DynamicDocs = (): string => {
 export default function Footer() {
 	return (
 		<div className="md:flex md:grid-rows-2 justify-items-center md:px-12 md:pb-[80px] max-md:pb-[40px] pt-6 bg-layout-footer text-layout-primary">
-			<div className="flex-1 justify-center">
+			<div className="md:flex-1 max-md:flex justify-center">
 				<SubmitIssue />
 			</div>
 
