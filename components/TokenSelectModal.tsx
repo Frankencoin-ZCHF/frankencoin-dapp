cd0f5d4f7b07bc8809ef9b9d383e1969126474c8
Daniel Padrino <danswarrior1@gmail.com>
2025-05-16 23:44:09 -0300

    Fix token selection modal

--- components/TokenSelectModal.tsx
+++ components/TokenSelectModal.tsx
@@ -54,17 +54,17 @@ export function TokenSelectModal({ title, isOpen, setIsOpen, children }: TokenSe
 			size="md"
 			theme={{
 				root: {
-					base: "fixed inset-x-0 top-0 z-50 h-screen overflow-y-auto overflow-x-hidden md:inset-0 md:h-full",
+					base: "fixed px-2 md:px-0 inset-x-0 top-0 z-50 h-screen overflow-y-auto overflow-x-hidden md:inset-0 md:h-full",
 				},
 				content: {
-					base: "relative max-h-[80%] w-full md:max-h-[80%]",
-					inner: "relative flex h-full flex-col rounded-lg bg-white shadow dark:bg-gray-700",
+					base: "relative max-h-[80%] w-full md:max-h-[80%] overflow-y-auto rounded-lg bg-white shadow dark:bg-gray-700",
+					inner: "relative flex h-full flex-col",
 				},
 			}}
 		>
 			<Modal.Header
 				theme={{
-					base: "flex items-center justify-between rounded-t px-6 pt-2 pb-0",
+					base: "flex items-center justify-between rounded-t px-6 pt-2 pb-0 sticky top-0 bg-white dark:bg-gray-700 z-10",
 					title: "text-lg font-extrabold leading-tight align-middle",
 					close: {
 						base: "p-1.5 pr-0 ml-auto inline-flex items-center rounded-lg bg-transparent",
@@ -74,7 +74,7 @@ export function TokenSelectModal({ title, isOpen, setIsOpen, children }: TokenSe
 			>
 				<div className="text-lg font-extrabold leading-tight align-middle">{title}</div>
 			</Modal.Header>
-			<Modal.Body theme={{ base: "flex flex-col px-3 py-2 overflow-y-auto no-scrollbar" }}>
+			<Modal.Body theme={{ base: "flex flex-col px-3 py-2 overflow-y-scroll" }}>
 				<div className="h-full">{children}</div>
 			</Modal.Body>
 		</Modal>
