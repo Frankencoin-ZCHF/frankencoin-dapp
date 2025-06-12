54cadfc70ef324db7002ee13c5c5fc2c63cf5f95
samclassix <samclassix@proton.me>
2025-06-12 13:37:09 +0200

    feat: userBalance multichain

--- hooks/useUserBalance.ts
+++ hooks/useUserBalance.ts
@@ -1,45 +1,110 @@
 import { useAccount, useReadContracts } from "wagmi";
 import { decodeBigIntCall } from "@utils";
-import { ADDRESS, EquityABI, FrankencoinABI } from "@frankencoin/zchf";
-import { mainnet } from "viem/chains";
+import { ADDRESS, BridgedFrankencoinABI, EquityABI, FrankencoinABI } from "@frankencoin/zchf";
+import { arbitrum, avalanche, base, gnosis, mainnet, optimism, polygon, sonic } from "viem/chains";
+import { Address, zeroAddress } from "viem";
 
-export const useUserBalance = () => {
+export const useUserBalance = (account?: Address) => {
 	const { address } = useAccount();
 
-	const frankenContract = {
-		address: ADDRESS[mainnet.id].frankencoin,
-		abi: FrankencoinABI,
-	} as const;
-
-	const equityContract = {
-		address: ADDRESS[mainnet.id].equity,
-		abi: EquityABI,
-	};
-
-	const account = address || "0x0";
+	if (!account) account = address || zeroAddress;
 
 	// Fetch all blockchain stats in one web3 call using multicall
 	const { data, isError, isLoading } = useReadContracts({
 		contracts: [
-			// Frankencoin Calls
+			// mainnet calls
 			{
-				...frankenContract,
+				address: ADDRESS[mainnet.id].frankencoin,
+				chainId: mainnet.id,
+				abi: FrankencoinABI,
 				functionName: "balanceOf",
 				args: [account],
 			},
 			{
-				...equityContract,
+				address: ADDRESS[mainnet.id].equity,
+				chainId: mainnet.id,
+				abi: EquityABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			// side chain calls
+			{
+				address: ADDRESS[polygon.id].ccipBridgedFrankencoin,
+				chainId: polygon.id,
+				abi: BridgedFrankencoinABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			{
+				address: ADDRESS[arbitrum.id].ccipBridgedFrankencoin,
+				chainId: arbitrum.id,
+				abi: BridgedFrankencoinABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			{
+				address: ADDRESS[optimism.id].ccipBridgedFrankencoin,
+				chainId: optimism.id,
+				abi: BridgedFrankencoinABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			{
+				address: ADDRESS[base.id].ccipBridgedFrankencoin,
+				chainId: base.id,
+				abi: BridgedFrankencoinABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			{
+				address: ADDRESS[avalanche.id].ccipBridgedFrankencoin,
+				chainId: avalanche.id,
+				abi: BridgedFrankencoinABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			{
+				address: ADDRESS[gnosis.id].ccipBridgedFrankencoin,
+				chainId: gnosis.id,
+				abi: BridgedFrankencoinABI,
+				functionName: "balanceOf",
+				args: [account],
+			},
+			{
+				address: ADDRESS[sonic.id].ccipBridgedFrankencoin,
+				chainId: sonic.id,
+				abi: BridgedFrankencoinABI,
 				functionName: "balanceOf",
 				args: [account],
 			},
 		],
 	});
 
-	const frankenBalance: bigint = data ? decodeBigIntCall(data[0]) : BigInt(0);
-	const equityBalance: bigint = data ? decodeBigIntCall(data[1]) : BigInt(0);
-
 	return {
-		frankenBalance,
-		equityBalance,
+		[mainnet.id]: {
+			frankencoin: data ? decodeBigIntCall(data[0]) : BigInt(0),
+			equity: data ? decodeBigIntCall(data[1]) : BigInt(0),
+		},
+		[polygon.id]: {
+			frankencoin: data ? decodeBigIntCall(data[2]) : BigInt(0),
+		},
+		[arbitrum.id]: {
+			frankencoin: data ? decodeBigIntCall(data[3]) : BigInt(0),
+		},
+		[optimism.id]: {
+			frankencoin: data ? decodeBigIntCall(data[4]) : BigInt(0),
+		},
+		[base.id]: {
+			frankencoin: data ? decodeBigIntCall(data[5]) : BigInt(0),
+		},
+		[avalanche.id]: {
+			frankencoin: data ? decodeBigIntCall(data[6]) : BigInt(0),
+		},
+		[gnosis.id]: {
+			frankencoin: data ? decodeBigIntCall(data[7]) : BigInt(0),
+		},
+		[sonic.id]: {
+			frankencoin: data ? decodeBigIntCall(data[8]) : BigInt(0),
+		},
 	};
 };
