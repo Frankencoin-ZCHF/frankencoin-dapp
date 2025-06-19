af714ade7114d858afe562478c97f5719c1c9dda
samclassix <samclassix@proton.me>
2025-06-19 16:19:35 +0200

    feat: guard with chainId

--- components/Guards/GuardSupportedChain.tsx
+++ components/Guards/GuardSupportedChain.tsx
@@ -2,29 +2,36 @@ import React, { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { useAppKit, useAppKitState, useAppKitNetwork } from "@reown/appkit/react";
 import Button from "@components/Button";
-import { WAGMI_CHAIN } from "../../app.config";
+import { WAGMI_CHAIN, WAGMI_CHAINS } from "../../app.config";
 import { AppKitNetwork } from "@reown/appkit/networks";
+import { ChainId } from "@frankencoin/zchf";
 
 interface Props {
 	children?: React.ReactNode;
 	label?: string;
 	disabled?: boolean;
 	chain?: AppKitNetwork;
+	chainId?: ChainId;
 }
 
-export default function GuardSupportedChain({ children, label, disabled, chain }: Props) {
+export default function GuardSupportedChain({ children, label, disabled, chain, chainId }: Props) {
 	const [requestedChange, setRequestedChange] = useState(false);
 
-	const { isDisconnected, chainId } = useAccount();
+	const Account = useAccount();
 	const AppKit = useAppKit();
 	const AppKitState = useAppKitState();
 	const AppKitNetwork = useAppKitNetwork();
+
+	// search chain from supported chainId
+	if (chain == undefined && chainId != undefined) {
+		chain = WAGMI_CHAINS.find((c) => c.id == chainId) as AppKitNetwork;
+	}
 
 	// load default chain
 	if (chain == undefined) chain = WAGMI_CHAIN;
 
 	// correct chain?
-	const isCorrectChain = chain.id == chainId;
+	const isCorrectChain = chain.id == Account.chainId;
 
 	// to close modal after successful connection or change of chain
 	useEffect(() => {
@@ -35,7 +42,7 @@ export default function GuardSupportedChain({ children, label, disabled, chain }
 	}, [requestedChange, isCorrectChain, AppKit, AppKitState]);
 
 	// Check if wallet is disconnected
-	if (isDisconnected)
+	if (Account.isDisconnected)
 		return (
 			<Button
 				className="h-10"
