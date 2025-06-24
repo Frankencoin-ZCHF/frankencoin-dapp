669dd18c3bea4dbe87335243ff7512c6bb670b21
lapatric <42653152+lapatric@users.noreply.github.com>
2025-06-24 09:51:20 +0200

    [NO-TASK] add approval before bid

--- pages/challenges/[index]/bid.tsx
+++ pages/challenges/[index]/bid.tsx
@@ -29,9 +29,11 @@ export default function ChallengePlaceBid() {
 	const [isInit, setInit] = useState(false);
 	const [amount, setAmount] = useState(0n);
 	const [error, setError] = useState("");
+	const [isApproving, setIsApproving] = useState(false);
 	const [isBidding, setBidding] = useState(false);
 	const [isNavigating, setNavigating] = useState(false);
 	const [userBalance, setUserBalance] = useState(0n);
+	const [userAllowance, setUserAllowance] = useState(0n);
 	const [auctionPrice, setAuctionPrice] = useState<bigint>(0n);
 
 	const { data } = useBlockNumber({ watch: true });
@@ -65,6 +67,14 @@ export default function ChallengePlaceBid() {
 					args: [acc],
 				});
 				setUserBalance(_balance);
+
+				const _allowance = await readContract(WAGMI_CONFIG, {
+					address: ADDR.decentralizedEURO,
+					abi: erc20Abi,
+					functionName: "allowance",
+					args: [acc, ADDR.mintingHubGateway],
+				});
+				setUserAllowance(_allowance);
 			}
 
 			const _price = await readContract(WAGMI_CONFIG, {
@@ -118,12 +128,56 @@ export default function ChallengePlaceBid() {
 		const valueBigInt = BigInt(value);
 		setAmount(valueBigInt);
 
-		if (expectedDEURO() > userBalance) {
+		const expectedAmount = expectedDEURO(valueBigInt);
+		
+		if (expectedAmount > userBalance) {
 			setError(t("challenges.error.not_enough_deuro", { symbol: TOKEN_SYMBOL }));
 		} else if (valueBigInt > remainingSize) {
 			setError(t("challenges.error.expected_winning_collateral"));
 		} else {
 			setError("");
+		}
+	};
+
+	const handleApprove = async () => {
+		try {
+			setIsApproving(true);
+
+			const approveWriteHash = await writeContract(WAGMI_CONFIG, {
+				address: ADDRESS[chainId].decentralizedEURO,
+				abi: erc20Abi,
+				functionName: "approve",
+				args: [ADDRESS[chainId].mintingHubGateway, maxUint256],
+			});
+
+			const toastContent = [
+				{
+					title: t("common.txs.amount"),
+					value: "infinite " + TOKEN_SYMBOL,
+				},
+				{
+					title: t("common.txs.spender"),
+					value: shortenAddress(ADDRESS[chainId].mintingHubGateway),
+				},
+				{
+					title: t("common.txs.transaction"),
+					hash: approveWriteHash,
+				},
+			];
+
+			await toast.promise(waitForTransactionReceipt(WAGMI_CONFIG, { hash: approveWriteHash, confirmations: 1 }), {
+				pending: {
+					render: <TxToast title={t("common.txs.title", { symbol: TOKEN_SYMBOL })} rows={toastContent} />,
+				},
+				success: {
+					render: <TxToast title={t("common.txs.success", { symbol: TOKEN_SYMBOL })} rows={toastContent} />,
+				},
+			});
+			setUserAllowance(maxUint256);
+		} catch (error) {
+			toast.error(renderErrorTxToast(error));
+		} finally {
+			setIsApproving(false);
 		}
 	};
 
@@ -255,14 +309,24 @@ export default function ChallengePlaceBid() {
 							</AppBox>
 						</div>
 						<div className="mx-auto mt-4 w-72 max-w-full flex-col">
-							<GuardToAllowedChainBtn label={t("common.buy")}>
-								<Button
-									disabled={amount == 0n || expectedDEURO() > userBalance || error != ""}
-									isLoading={isBidding}
-									onClick={() => handleBid()}
-								>
-									{t("common.buy")}
-								</Button>
+							<GuardToAllowedChainBtn label={userAllowance < expectedDEURO() ? t("common.approve") : t("common.buy")}>
+								{userAllowance < expectedDEURO() ? (
+									<Button
+										disabled={amount == 0n || error != ""}
+										isLoading={isApproving}
+										onClick={() => handleApprove()}
+									>
+										{t("common.approve")}
+									</Button>
+								) : (
+									<Button
+										disabled={amount == 0n || expectedDEURO() > userBalance || error != ""}
+										isLoading={isBidding}
+										onClick={() => handleBid()}
+									>
+										{t("common.buy")}
+									</Button>
+								)}
 							</GuardToAllowedChainBtn>
 						</div>
 					</div>
