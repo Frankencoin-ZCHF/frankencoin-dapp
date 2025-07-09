c25f7532a6ad18dee04ba34005ed9a291663a7d0
samclassix <samclassix@proton.me>
2025-07-09 14:47:19 +0200

    fix:  Repeated forward-slashes (//) using path.join

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,18 +1,19 @@
 import { Chain, Hash } from "viem";
 import { CONFIG, WAGMI_CHAIN } from "../app.config";
+import path from "path";
 
-export const AppUrl = (path: string, chain: Chain = WAGMI_CHAIN) => {
-	return CONFIG.app + path;
+export const AppUrl = (url: string) => {
+	return path.join(CONFIG.app, url);
 };
 
 export const ContractUrl = (address: string, chain: Chain = WAGMI_CHAIN) => {
-	const explorerLink = chain?.blockExplorers?.default.url || "https://etherscan.io";
-	return explorerLink + "/address/" + address;
+	const explorerLink = chain?.blockExplorers?.default.url || "https://etherscan.io/";
+	return path.join(explorerLink, "address", address);
 };
 
 export const TxUrl = (hash: Hash, chain: Chain = WAGMI_CHAIN) => {
-	const explorerLink = chain?.blockExplorers?.default.url || "https://etherscan.io";
-	return explorerLink + "/tx/" + hash;
+	const explorerLink = chain?.blockExplorers?.default.url || "https://etherscan.io/";
+	return path.join(explorerLink, "tx", hash);
 };
 
 export const MorphoMarketUrl = (id: string) => `https://app.morpho.org/ethereum/market/${id}`;
