a95e0ed924c2853ad5df92ef749120fa16972868
samclassix <samclassix@proton.me>
2025-09-16 12:39:06 +0200

    fix: query chain for savings module

--- pages/savings.tsx
+++ pages/savings.tsx
@@ -1,54 +1,54 @@
 import SavingsGlobalCard from "@components/PageSavings/SavingsGlobalCard";
 import SavingsInteractionCard from "@components/PageSavings/SavingsInteractionCard";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { store } from "../redux/redux.store";
+import { useEffect } from "react";
+import { RootState, store } from "../redux/redux.store";
 import { fetchLeadrate, fetchSavings } from "../redux/slices/savings.slice";
-import { useAccount } from "wagmi";
+import { useAccount, useChainId } from "wagmi";
 import AppTitle from "@components/AppTitle";
 import SavingsRankedBalancesTable from "@components/PageSavings/SavingsRankedBalancesTable";
 import AppLink from "@components/AppLink";
 import SavingsRecentActivitiesTable from "@components/PageSavings/SavingsRecentActivitiesTable";
 import { useRouter } from "next/router";
 import { Address, isAddress, zeroAddress } from "viem";
-import { ApiSavingsActivity } from "@frankencoin/api";
-import { FRANKENCOIN_API_CLIENT } from "../app.config";
 import ReportsYearlyTable from "@components/PageReports/ReportsSavingsYearlyTable";
+import { useSelector } from "react-redux";
+import { getChainByName } from "@utils";
+import { useAppKitNetwork } from "@reown/appkit/react";
+import { ChainId } from "@frankencoin/zchf";
 
 export default function SavingsPage() {
-	const [savings, setSavings] = useState<ApiSavingsActivity>([]);
+	const activities = useSelector((state: RootState) => state.savings.savingsActivity);
 	const { address } = useAccount();
 	const router = useRouter();
+	const AppKitNetwork = useAppKitNetwork();
+	const chainId = useChainId() as ChainId;
 
 	const queryAddress: Address = String(router.query.address).toLowerCase() as Address;
 	const account = isAddress(queryAddress) ? queryAddress : address ?? zeroAddress;
 
+	const queryChain: string = String(router.query.chain).toLowerCase();
+
 	useEffect(() => {
 		store.dispatch(fetchLeadrate());
-		store.dispatch(fetchSavings(account));
+		store.dispatch(fetchSavings(account == zeroAddress ? undefined : account));
 	}, [account]);
 
 	useEffect(() => {
-		if (account.length == 0) {
-			return;
+		let targetChainToCheck = queryChain.toLowerCase();
+
+		if (targetChainToCheck == "optimism") {
+			targetChainToCheck = "OP Mainnet";
+		} else if (targetChainToCheck == "arbitrum") {
+			targetChainToCheck = "Arbitrum One";
 		}
 
-		if (!isAddress(account)) {
-			setSavings([]);
-			return;
+		const targetChain = getChainByName(targetChainToCheck);
+
+		if (targetChain.id != chainId) {
+			AppKitNetwork.switchNetwork(targetChain);
 		}
-
-		const fetcher = async () => {
-			try {
-				const responseSavings = await FRANKENCOIN_API_CLIENT.get(`/savings/core/activity/${account}`);
-				setSavings(responseSavings.data as ApiSavingsActivity);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-
-		fetcher();
-	}, [account]);
+	}, [chainId, queryChain, AppKitNetwork]);
 
 	return (
 		<>
@@ -83,9 +83,9 @@ export default function SavingsPage() {
 					<AppLink className="" label={" report page"} href={`/report`} />.
 				</div>
 			</AppTitle>
-			<ReportsYearlyTable activity={savings} />
+			<ReportsYearlyTable activity={activities} />
 
-			<AppTitle title={account == undefined ? "Recent Activities" : "Your latest Activities"} />
+			<AppTitle title={account == undefined || account == zeroAddress ? "Recent Activities" : "Your latest Activities"} />
 
 			<SavingsRecentActivitiesTable />
 
