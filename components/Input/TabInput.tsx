6595e2ad97300f2abb94bb0ecb62848175fd4eba
samclassix <samclassix@proton.me>
2026-03-30 11:20:38 +0200

    feat: restore components and adjust style

--- components/Input/TabInput.tsx
+++ components/Input/TabInput.tsx
@@ -4,25 +4,26 @@ export interface TabInputInterface {
 	tabs?: string[];
 	tab?: string;
 	setTab?: Dispatch<SetStateAction<string>>;
-	paddingX?: string;
 }
 
-export function TabInput({ tabs = [], tab = "", setTab = () => {}, paddingX = "6" }: TabInputInterface) {
+export function TabInput({ tabs = [], tab = "", setTab = () => {} }: TabInputInterface) {
 	if (tabs.length == 0) return null;
 
 	return (
-		<div className="flex flex-row justify-between text-text-secondary">
-			{tabs.map((ts) => (
-				<div
-					key={"key_" + ts}
-					className={`rounded-lg bg-card-content-primary px-${paddingX} max-md:px-4 py-2 text-sm text-center ${
-						ts == tab ? "text-text-primary font-semibold" : "cursor-pointer hover:text-button-hover hover:font-semibold"
-					}`}
-					onClick={() => setTab(ts)}
-				>
-					{ts}
-				</div>
-			))}
+		<div className="bg-card-content-primary mb-5 rounded-lg">
+			<div className="flex flex-row justify-between px-6 text-text-secondary">
+				{tabs.map((ts) => (
+					<div
+						key={"key_" + ts}
+						className={`px-6 max-md:px-2 py-2 text-sm text-center ${
+							ts == tab ? "text-text-primary font-semibold" : "cursor-pointer"
+						}`}
+						onClick={() => setTab(ts)}
+					>
+						{ts}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
