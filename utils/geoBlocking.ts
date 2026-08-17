79b79b2213acb700107eb0ce3dc245492c8b214c
samclassix <samclassix@proton.me>
2026-08-17 14:53:14 +0200

    feat: added more geo blocking regions

--- utils/geoBlocking.ts
+++ utils/geoBlocking.ts
@@ -6,8 +6,28 @@
 import { normalizeAddress } from "./format";
 
 // into the edge middleware.
+//
+// SPYon list is Ondo's "Jurisdiction-Based Prohibitions" (Ondo GM tokens). Country-level
+// only, since cf-ipcountry can't resolve sub-national regions - the Crimea/DNR/LNR/Kherson/
+// Zaporizhzhia/Sevastopol entries on that list have no ISO country code and aren't covered
+// here; they'd need separate enforcement (e.g. blocking wallet addresses on an OFAC list).
 const COLLATERAL_GEO_BLOCKS: Record<string, string[]> = {
-	"0xfedc5f4a6c38211c1338aa411018dfaf26612c08": ["US"], // SPYon - SPDR S&P 500 ETF (Ondo Tokenized)
+	"0xfedc5f4a6c38211c1338aa411018dfaf26612c08": [
+		"AF", // Afghanistan
+		"BY", // Belarus
+		"CA", // Canada
+		"CU", // Cuba
+		"KP", // Democratic People's Republic of Korea
+		"IR", // Iran
+		"LY", // Libya
+		"MM", // Myanmar
+		"RU", // Russia
+		"SO", // Somalia
+		"SS", // South Sudan
+		"SD", // Sudan
+		"SY", // Syria
+		"US", // United States (incl. all territories & federal districts)
+	], // SPYon - SPDR S&P 500 ETF (Ondo Tokenized)
 };
 
 export function getBlockedCountriesForCollateral(address: string): string[] {
